9407bddc40e8f6b8078eebd83d9226c773075eea
Bartosz Ejman <ejman.bartosz@gmail.com>
2024-08-20 10:17:14 +0200

    implementation the instructor's suggestions

--- js/converter.js
+++ js/converter.js
@@ -1,26 +1,39 @@
 let amountElement = document.querySelector(".js-form__amount");
-console.log(amountElement);
-let rateElement = document.querySelector(".js-form__rate")
-console.log(rateElement);
-let formElement = document.querySelector(".js-form")
-console.log(formElement);
-let resultElement = document.querySelector(".js-form__result")
-console.log(resultElement);
-let currencyElement = document.querySelector(".js-form__currency")
-console.log(currencyElement);
+let rateElement = document.querySelector(".js-form__rate");
+let formElement = document.querySelector(".js-form");
+let resultElement = document.querySelector(".js-form__result");
+let currencyElement = document.querySelector(".js-form__currency");
+
+currencyElement.addEventListener("change", (event) => {
+
+  switch (currencyElement.value) {
+    case 'USD':
+      rateElement.value = '0,2566';
+      break;
+    case 'EUR':
+      rateElement.value = '0,2333';
+      break;
+    case 'CHF':
+      rateElement.value = '0,2218';
+      break;
+    case 'GBP':
+      rateElement.value = '0,1994';
+      break;
+    default:
+      rateElement.value = '';
+      break;
+  }
+})
 
 formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
 
-    let amount = amountElement.value;
-    let rate = rateElement.value;
-    let currency = currencyElement.value;
+  let amount = amountElement.value;
+  let rate = rateElement.value;
+  let currency = currencyElement.value;
 
-    let result = amount * rate;
+  let result = amount * rate;
 
-    console.log(result);
-    console.log(currency);
-
-    resultElement.innerText = `${result.toFixed(2)} ${currency}` ;
+  resultElement.innerText = `${result.toFixed(2)} ${currency}`;
 });
