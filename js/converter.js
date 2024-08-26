eb9d6d8c13f57dac7db781365f892653534d17fb
Bartosz Ejman <ejman.bartosz@gmail.com>
2024-08-26 18:31:39 +0200

    JS organizing

--- js/converter.js
+++ js/converter.js
@@ -1,39 +1,57 @@
-let amountElement = document.querySelector(".js-form__amount");
-let rateElement = document.querySelector(".js-form__rate");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-form__result");
-let currencyElement = document.querySelector(".js-form__currency");
+{
+  const rateElement = document.querySelector(".js-form__rate");
+  const currencyElement = document.querySelector(".js-form__currency");
 
-currencyElement.addEventListener("change", (event) => {
+  const enterTheRate = () => {
 
-  switch (currencyElement.value) {
-    case 'USD':
-      rateElement.value = '0,2566';
-      break;
-    case 'EUR':
-      rateElement.value = '0,2333';
-      break;
-    case 'CHF':
-      rateElement.value = '0,2218';
-      break;
-    case 'GBP':
-      rateElement.value = '0,1994';
-      break;
-    default:
-      rateElement.value = '';
-      break;
+    switch (currencyElement.value) {
+      case 'USD':
+        rateElement.value = '3.8570';
+        break;
+      case 'EUR':
+        rateElement.value = '4.2820';
+        break;
+      case 'CHF':
+        rateElement.value = '4.5256';
+        break;
+      case 'GBP':
+        rateElement.value = '5.0459';
+        break;
+      default:
+        rateElement.value = '';
+        break;
+    }
   }
-})
-
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
 
 
-  let amount = amountElement.value;
-  let rate = rateElement.value;
-  let currency = currencyElement.value;
+  const onFormSubmit = (event) => {
+    event.preventDefault();
 
-  let result = amount * rate;
+    const amountElement = document.querySelector(".js-form__amount");
 
-  resultElement.innerText = `${result.toFixed(2)} ${currency}`;
-});
+    const amount = amountElement.value;
+    const rate = rateElement.value;
+    const currency = currencyElement.value;
+
+    const result = amount / rate;
+    showResult(result, currency);
+
+  };
+
+
+  const showResult = (result, currency) => {
+    const resultElement = document.querySelector(".js-form__result");
+    resultElement.innerText = `${result.toFixed(2)} ${currency}`;
+  }
+
+
+  const init = () => {
+    currencyElement.addEventListener("change", enterTheRate);
+
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", onFormSubmit);
+  }
+
+  init();
+
+}
